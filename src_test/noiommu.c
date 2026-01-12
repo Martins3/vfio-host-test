d1a7d456b55f8ac7f6b1b48090c7535ea81fecb7
Xueshi Hu <xueshi.hu@smartx.com>
2026-01-12 09:56:00 +0800

    💯
    Signed-off-by: Xueshi Hu <xueshi.hu@smartx.com>

--- src_test/noiommu.c
+++ src_test/noiommu.c
@@ -10,80 +10,95 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-int main() {
-  /* 假设设备 group 是 noiommu-25 */
-  const char *group_path = "/dev/vfio/noiommu-0";
-  const char *device_name = "0000:00:03.0";
+int main()
+{
+	/*
+	 * 先需要打开
+	 * echo 1 | sudo tee /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
+	 *
+	 * 需要借助 code/qemu/vfio.sh 来配置
+	 *
+	 * echo 0000:c8:00.0 | sudo tee /sys/bus/pci/drivers/hct/bind
+	 *
+	 * 假设设备 group 是 noiommu-25
+	 *
+	 * 我当时是在虚拟机中测试的吗? 仅仅 enable_unsafe_noiommu_mode 是没用的
+	 *
+	 * 但是从 vfio_ioctl_set_iommu 看，的确是动态可以配置的
+	 *
+	 */
+	const char *group_path = "/dev/vfio/noiommu-0";
+	const char *device_name = "0000:c8:00.0";
 
-  int container = open("/dev/vfio/vfio", O_RDWR);
-  if (container < 0) {
-    perror("open container");
-    return 1;
-  }
+	int container = open("/dev/vfio/vfio", O_RDWR);
+	if (container < 0) {
+		perror("open container");
+		return 1;
+	}
 
-  if (ioctl(container, VFIO_GET_API_VERSION) != VFIO_API_VERSION) {
-    printf("VFIO API mismatch\n");
-    return 1;
-  }
+	if (ioctl(container, VFIO_GET_API_VERSION) != VFIO_API_VERSION) {
+		printf("VFIO API mismatch\n");
+		return 1;
+	}
 
-  /* 打开 noiommu group */
-  int group = open(group_path, O_RDWR);
-  if (group < 0) {
-    perror("open group");
-    return 1;
-  }
+	/* 打开 noiommu group */
+	int group = open(group_path, O_RDWR);
+	if (group < 0) {
+		perror("open group");
+		return 1;
+	}
 
-  if (ioctl(group, VFIO_GROUP_SET_CONTAINER, &container)) {
-    perror("SET_CONTAINER");
-    return 1;
-  }
+	if (ioctl(group, VFIO_GROUP_SET_CONTAINER, &container)) {
+		perror("SET_CONTAINER");
+		return 1;
+	}
 
-  /* 分配 noiommu container 类型 */
-  int t = VFIO_NOIOMMU_IOMMU;
-  if (ioctl(container, VFIO_SET_IOMMU, t)) {
-    perror("SET_IOMMU (noiommu)");
-    return 1;
-  }
+	/* 分配 noiommu container 类型 */
+	int t = VFIO_NOIOMMU_IOMMU;
+	if (ioctl(container, VFIO_SET_IOMMU, t)) {
+		perror("SET_IOMMU (noiommu)");
+		return 1;
+	}
 
-  /* 获取设备句柄 */
-  int dev = ioctl(group, VFIO_GROUP_GET_DEVICE_FD, device_name);
-  if (dev < 0) {
-    perror("GET_DEVICE_FD");
-    return 1;
-  }
+	/* 获取设备句柄 */
+	int dev = ioctl(group, VFIO_GROUP_GET_DEVICE_FD, device_name);
+	if (dev < 0) {
+		perror("GET_DEVICE_FD");
+		return 1;
+	}
 
-  printf("\n=== Regions of device %s ===\n\n", device_name);
+	printf("\n=== Regions of device %s ===\n\n", device_name);
 
-  /* 枚举 region */
-  for (uint32_t i = 0;; i++) {
-    struct vfio_region_info reg = {
-        .argsz = sizeof(reg),
-        .index = i,
-    };
+	/* 枚举 region */
+	for (uint32_t i = 0;; i++) {
+		struct vfio_region_info reg = {
+			.argsz = sizeof(reg),
+			.index = i,
+		};
 
-    if (ioctl(dev, VFIO_DEVICE_GET_REGION_INFO, &reg) < 0)
-      break; // 没有更多 region
-	     //
-    if(reg.size == 0)
-	    continue;
+		// 没有更多 region
+		if (ioctl(dev, VFIO_DEVICE_GET_REGION_INFO, &reg) < 0)
+			break;
+		if (reg.size == 0)
+			continue;
 
-    printf("Region %u:\n", i);
-    printf("  size   : 0x%llx\n", (unsigned long long)reg.size);
-    printf("  offset : 0x%llx\n", (unsigned long long)reg.offset);
-    printf("  flags  : 0x%x\n", reg.flags);
+		printf("Region %u:\n", i);
+		printf("  size   : 0x%llx\n", (unsigned long long)reg.size);
+		printf("  offset : 0x%llx\n", (unsigned long long)reg.offset);
+		printf("  flags  : 0x%x\n", reg.flags);
 
-    if (reg.flags & VFIO_REGION_INFO_FLAG_READ)
-      printf("    - READ\n");
-    if (reg.flags & VFIO_REGION_INFO_FLAG_WRITE)
-      printf("    - WRITE\n");
-    if (reg.flags & VFIO_REGION_INFO_FLAG_MMAP)
-      printf("    - MMAP capable\n");
+		if (reg.flags & VFIO_REGION_INFO_FLAG_READ)
+			printf("    - READ\n");
+		if (reg.flags & VFIO_REGION_INFO_FLAG_WRITE)
+			printf("    - WRITE\n");
+		if (reg.flags & VFIO_REGION_INFO_FLAG_MMAP)
+			printf("    - MMAP capable\n");
 
-    printf("\n");
-  }
+		printf("\n");
+	}
 
-  close(dev);
-  close(group);
-  close(container);
-  return 0;
+	close(dev);
+	close(group);
+	close(container);
+	return 0;
 }
